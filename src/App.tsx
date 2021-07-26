8f6841c5061c47d440ed8e2a61ab1048729b3a6c
Vinicios Amaral <vinicios.amaralth@gmail.com>
2021-07-25 21:14:44 -0300

    NETFLIX 43:00

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { async } from 'q';
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
 
 function App() {
+const [movieList, setMovieList] = useState([]);
+
+type typelist = slug | title | items
+
+  useEffect( () =>{
+      const loadAll = async () => {
+          //pegando a lista total
+        let list = await Tmdb.getHomeList();
+        setMovieList(list: typelist);
+      }
+      loadAll();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>oii</h1>
     </div>
   );
 }
---
+++ src/Tmdb.ts
@@ -0,0 +1,64 @@
+const API_KEY = '38c007f28d5b66f36b9c3cf8d8452a4b';
+const API_BASE = 'http://api.themoviedb.org/3';
+
+// originais netflix
+// recomendados - trnding
+// em alta top rated
+//  acao
+// comedia
+// terror
+// romance
+//documentarios
+
+const basicFetch = async (endpoint: string) => {
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: 'Originais do Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'tremding',
+                title: 'Recomendados para voce',
+                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'toprated',
+                title: 'Em alta',
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title: 'Acao',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'comedy',
+                title: 'Comedia',
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentarios',
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+            },
+        ]
+    }
+}

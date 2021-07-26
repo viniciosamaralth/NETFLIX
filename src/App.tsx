af5c320e30c946a9987d28974764c93ada55818c
Vinicios Amaral <vinicios.amaral@agilsoft.local>
2021-07-26 17:50:22 -0300

    NETFLIXES

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import { async } from 'q';
 import React, { useEffect, useState } from 'react';
-import { string } from 'yargs';
 import Tmdb from './Tmdb';
 import './app.css';
 
 
 function App() {
-const [movieList, setMovieList] = useState([]);
+const [movieList, setMovieList] = useState<Array<Object>>([]);
 
   useEffect( () =>{
       const loadAll = async () => {
           //pegando a lista total
         let list = await Tmdb.getHomeList();
-        setMovieList(list);
+        setMovieList(movieList => [...movieList, list]);
       }
       loadAll();
   }, []);
 
-  return (
+  return (    
     <div className="App">
       <section className="lists">
         {movieList.map((item, key)=>(
@@ -29,5 +27,6 @@ const [movieList, setMovieList] = useState([]);
     </div>
   );
 }
+
 
 export default App;
